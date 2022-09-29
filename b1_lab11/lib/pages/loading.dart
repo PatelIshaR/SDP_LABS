cf74962ece9b7a388bd49bfe67ed41f84fd81dc9
isha <ishapatel1902@gmail.com>
2022-09-29 18:44:41 +0530

    lab11 added

---
+++ b1_lab11/lib/pages/loading.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:b1_lab11/services/world_time.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+
+class Loading extends StatefulWidget {
+  @override
+  State<Loading> createState() => _LoadingState();
+}
+class _LoadingState extends State<Loading> {
+  // String? time = 'LOADING..........';
+  void setWorldTime() async {
+    WordTime timeinstance =
+    WordTime(location: 'kolkata',flag: 'india.png',url: 'Asia/Kolkata');
+     await timeinstance.getTime();
+      //Navigator.pushNamed(context, '/home');
+     Navigator.pushReplacementNamed(context, '/home', arguments: {
+       'location' : timeinstance.location,
+       'flag' : timeinstance.flag,
+       'time' : timeinstance.time,
+     });
+// // print(timeinstance.time);
+//     setState(() {
+//       time = timeinstance.time;
+//     });
+  }
+  @override
+  void initState() {
+    super.initState();
+    setWorldTime();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blueGrey,
+      body: Center(
+          child: SpinKitFadingCube(
+            color: Colors.white,
+            size: 50.0,
+          ),
+          )
+      );
+        // body: Padding(
+        //   padding: EdgeInsets.all(60.0),
+        //   child: Text('loading..'),
+        // )
+    // );
+  }
+}
